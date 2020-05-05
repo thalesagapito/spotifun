bbe4def594a2b4d810aac16b92ded7f90e8b6674
Giulia Padovani <giuliacpadovani@gmail.com>
2020-05-04 23:33:49 -0300

    Add SpotifyEx dependency

--- spotifun-api/mix.exs
+++ spotifun-api/mix.exs
@@ -10,7 +10,7 @@ defmodule Spotifun.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
     ]
   end
 
@@ -44,7 +44,8 @@ defmodule Spotifun.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:spotify_ex, "~> 2.0.9"}
     ]
   end
 
